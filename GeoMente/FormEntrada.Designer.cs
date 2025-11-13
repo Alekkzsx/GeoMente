88693e4bb86acfc14b8eb8d40f3d307841d70615
Alekkzsx <alex.gss@puccampinas.edu.br>
2025-11-13 18:58:54 -0300

    video

--- GeoMente/FormEntrada.Designer.cs
+++ GeoMente/FormEntrada.Designer.cs
@@ -23,10 +23,29 @@ namespace GeoMente
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.SuspendLayout();
             // 
+            // webView21
+            // 
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(1356, 722);
+            this.webView21.TabIndex = 4;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // panelCentral
+            // 
+            this.panelCentral = new System.Windows.Forms.Panel();
+            this.panelCentral.SuspendLayout();
+            this.SuspendLayout();
+            // 
             // pictureBoxLogo
             // 
             this.pictureBoxLogo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.pictureBoxLogo.Location = new System.Drawing.Point(448, 150);
+            this.pictureBoxLogo.Location = new System.Drawing.Point(0, 0);
             this.pictureBoxLogo.Name = "pictureBoxLogo";
             this.pictureBoxLogo.Size = new System.Drawing.Size(460, 200);
             this.pictureBoxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -36,12 +55,12 @@ namespace GeoMente
             // btnJogar
             // 
             this.btnJogar.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnJogar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(191)))), ((int)(((byte)(255))))); // DeepSkyBlue
+            this.btnJogar.BackColor = System.Drawing.Color.DeepSkyBlue;
             this.btnJogar.FlatAppearance.BorderSize = 0;
             this.btnJogar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnJogar.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnJogar.ForeColor = System.Drawing.Color.White;
-            this.btnJogar.Location = new System.Drawing.Point(578, 380);
+            this.btnJogar.Location = new System.Drawing.Point(0, 200);
             this.btnJogar.Name = "btnJogar";
             this.btnJogar.Size = new System.Drawing.Size(200, 60);
             this.btnJogar.TabIndex = 1;
@@ -51,30 +70,41 @@ namespace GeoMente
             // btnSair
             // 
             this.btnSair.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnSair.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(43)))), ((int)(((byte)(226))))); // BlueViolet
+            this.btnSair.BackColor = System.Drawing.Color.BlueViolet;
             this.btnSair.FlatAppearance.BorderSize = 0;
             this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSair.ForeColor = System.Drawing.Color.White;
-            this.btnSair.Location = new System.Drawing.Point(603, 460);
+            this.btnSair.Location = new System.Drawing.Point(0, 260);
             this.btnSair.Name = "btnSair";
             this.btnSair.Size = new System.Drawing.Size(150, 50);
             this.btnSair.TabIndex = 2;
             this.btnSair.Text = "Sair";
             this.btnSair.UseVisualStyleBackColor = false;
             // 
+            // panelCentral
+            // 
+            this.panelCentral.BackColor = System.Drawing.Color.FromArgb(100, 0, 0, 0);
+            this.panelCentral.Controls.Add(this.pictureBoxLogo);
+            this.panelCentral.Controls.Add(this.btnJogar);
+            this.panelCentral.Controls.Add(this.btnSair);
+            this.panelCentral.Location = new System.Drawing.Point(448, 150);
+            this.panelCentral.Name = "panelCentral";
+            this.panelCentral.Size = new System.Drawing.Size(460, 310);
+            this.panelCentral.TabIndex = 3;
+            // 
             // FormEntrada
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(27)))), ((int)(((byte)(42))))); // Dark blue background
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(27)))), ((int)(((byte)(42)))));
             this.ClientSize = new System.Drawing.Size(1356, 722);
-            this.Controls.Add(this.btnSair);
-            this.Controls.Add(this.btnJogar);
-            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.panelCentral);
+            this.Controls.Add(this.webView21);
             this.Name = "FormEntrada";
             this.Text = "Bem-vindo ao GeoMente";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.panelCentral.ResumeLayout(false);
             this.ResumeLayout(false);
         }
 
@@ -83,5 +113,7 @@ namespace GeoMente
         private System.Windows.Forms.PictureBox pictureBoxLogo;
         private System.Windows.Forms.Button btnJogar;
         private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Panel panelCentral;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
     }
 }
--- GeoMente/FormEntrada.cs
+++ GeoMente/FormEntrada.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.IO;
 using System.Reflection;
 using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
 
 namespace GeoMente
 {
@@ -36,6 +37,25 @@ namespace GeoMente
 
             // Centraliza os controles na inicialização
             CenterControls();
+
+            // Conecta os eventos MouseEnter e MouseLeave aos botões
+            btnJogar.MouseEnter += new EventHandler(btnJogar_MouseEnter);
+            btnJogar.MouseLeave += new EventHandler(btnJogar_MouseLeave);
+            btnSair.MouseEnter += new EventHandler(btnSair_MouseEnter);
+            btnSair.MouseLeave += new EventHandler(btnSair_MouseLeave);
+
+            // Configura o WebView2
+            var webView2Initialization = InitializeWebView2Async();
+        }
+
+        private async void InitializeWebView2Async()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.Navigate("https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&loop=1&rel=0&controls=0&showinfo=0");
+            webView21.CoreWebView2.Settings.IsWebBrowserContextMenuEnabled = false;
+            webView21.CoreWebView2.Settings.AreBrowserShortcutsEnabled = false;
+            webView21.CoreWebView2.Settings.IsStatusBarEnabled = false;
+            webView21.CoreWebView2.Settings.AreDevToolsEnabled = false;
         }
 
         private void FormEntrada_Resize(object sender, EventArgs e)
@@ -45,16 +65,20 @@ namespace GeoMente
 
         private void CenterControls()
         {
-            // Centraliza a Logo
-            pictureBoxLogo.Left = (this.ClientSize.Width - pictureBoxLogo.Width) / 2;
-            pictureBoxLogo.Top = (this.ClientSize.Height - pictureBoxLogo.Height) / 2 - 100; // Um pouco para cima
+            // Centraliza o painel central
+            panelCentral.Left = (this.ClientSize.Width - panelCentral.Width) / 2;
+            panelCentral.Top = (this.ClientSize.Height - panelCentral.Height) / 2;
+
+            // Centraliza a Logo dentro do painel
+            pictureBoxLogo.Left = (panelCentral.Width - pictureBoxLogo.Width) / 2;
+            pictureBoxLogo.Top = 0;
 
             // Centraliza o botão Jogar
-            btnJogar.Left = (this.ClientSize.Width - btnJogar.Width) / 2;
+            btnJogar.Left = (panelCentral.Width - btnJogar.Width) / 2;
             btnJogar.Top = pictureBoxLogo.Bottom + 20;
 
             // Centraliza o botão Sair
-            btnSair.Left = (this.ClientSize.Width - btnSair.Width) / 2;
+            btnSair.Left = (panelCentral.Width - btnSair.Width) / 2;
             btnSair.Top = btnJogar.Bottom + 10;
         }
 
@@ -71,6 +95,26 @@ namespace GeoMente
         {
             // Fecha a aplicação
             Application.Exit();
+        }
+
+        private void btnJogar_MouseEnter(object sender, EventArgs e)
+        {
+            btnJogar.BackColor = Color.FromArgb(90, 200, 255);
+        }
+
+        private void btnJogar_MouseLeave(object sender, EventArgs e)
+        {
+            btnJogar.BackColor = Color.DeepSkyBlue;
+        }
+
+        private void btnSair_MouseEnter(object sender, EventArgs e)
+        {
+            btnSair.BackColor = Color.FromArgb(150, 43, 226);
+        }
+
+        private void btnSair_MouseLeave(object sender, EventArgs e)
+        {
+            btnSair.BackColor = Color.BlueViolet;
         }
     }
 }
